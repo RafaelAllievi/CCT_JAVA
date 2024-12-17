05ae2fe63925f787c38ba151c2b4ea367f845a44
RafaelAllievi <130495465+RafaelAllievi@users.noreply.github.com>
2024-12-17 15:41:20 +0000

    Empty project

--- NetBeansProjects/Git_2024297/src/git_2024297/Git_2024297.java
+++ NetBeansProjects/Git_2024297/src/git_2024297/Git_2024297.java
@@ -15,7 +15,7 @@ public class Git_2024297 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Rafael-2024297");
+       
         
     }
     
